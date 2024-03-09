ae9c656e66515a1d308c4503467d804c828e527b
implaymo <136201112+implaymo@users.noreply.github.com>
2024-03-09 16:52:42 +0000

    Use nested loop to get more rows of blocks.

--- blocks.py
+++ blocks.py
@@ -3,15 +3,18 @@ import random
 
 class Block():
     def __init__(self) -> None:
-        self.block_height = 50
-        self.block_width = 51
+        self.block_height = 20
+        self.block_width = 81
+        self.rows = 4
+        self.columns = 15
         self.get_random_color()
         
     def draw_multiple_blocks(self,screen):
-        for i in range(23):
-            player_pos_x = 0 + i * (self.block_width + 5)
-            player_pos_y = 0
-            pygame.draw.rect(screen, self.color, pygame.Rect(player_pos_x, player_pos_y, self.block_width, self.block_height))
+        for row in range(self.rows):
+            for column in range(self.columns):
+                player_pos_x = 0 + column * (self.block_width + 5)
+                player_pos_y = 100 + row * (self.block_height + 5)
+                pygame.draw.rect(screen, self.color, pygame.Rect(player_pos_x, player_pos_y, self.block_width, self.block_height))
 
     def get_random_color(self):
         red = random.randint(0, 255)
