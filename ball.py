c0e5ad336bb611bec3e0acae67892633f7c0238d
implaymo <136201112+implaymo@users.noreply.github.com>
2024-03-11 20:30:23 +0000

    Fix ball bouncing on the bottom of screen without touching paddle. Fix limits so it only bounces when touches paddle.

--- ball.py
+++ ball.py
@@ -3,9 +3,10 @@ from paddle import PaddleRect
 
 class GameBall:
     def __init__(self) -> None:
-        self.ball_pos = pygame.Vector2(640, 690)
+        self.ball_pos = pygame.Vector2(640, 660)
         self.ball_color = "white"
         self.ball_speed = [0.5, 3]
+        
         
         
         
@@ -19,28 +20,31 @@ class GameBall:
     def ball_hit_left_side_paddle(self, paddle: PaddleRect):
         """Checks if ball hits the left side of the paddle and if it is between boundaries"""
         y_tolerance = 5
-        if (self.ball_pos.x <= paddle.paddle_pos.x + (paddle.paddle_width / 2) and paddle.paddle_pos.y - y_tolerance <= self.ball_pos.y <= paddle.paddle_pos.y + y_tolerance):
+        mid_paddle_pos = paddle.paddle_pos.x + (paddle.paddle_width / 2)
+        if (paddle.paddle_pos.x <= self.ball_pos.x <= mid_paddle_pos and paddle.paddle_pos.y - y_tolerance <= self.ball_pos.y <= paddle.paddle_pos.y + y_tolerance):
+            
             self.move_ball_diagonal_left_up()
-            print(f"PADDLE X POSITION {paddle.paddle_pos.x} ")
-            print(f"BALL X POSITION = {self.ball_pos.x}")
+
+        
     
 
     def ball_hit_right_side_paddle(self, paddle: PaddleRect):
         """Checks if ball hits the right side of the paddle and if it is between boundaries"""
         y_tolerance = 5
-        if (self.ball_pos.x >= paddle.paddle_pos.x + (paddle.paddle_width / 2) and paddle.paddle_pos.y - y_tolerance <= self.ball_pos.y <= paddle.paddle_pos.y + y_tolerance):
+        mid_paddle_pos = paddle.paddle_pos.x + (paddle.paddle_width / 2)
+        right_edge_paddle = paddle.paddle_pos.x + paddle.paddle_width
+        if (mid_paddle_pos <= self.ball_pos.x <= right_edge_paddle and paddle.paddle_pos.y - y_tolerance <= self.ball_pos.y <= paddle.paddle_pos.y + y_tolerance):
             
             self.move_ball_diagonal_right_up()
-            print(f"PADDLE X POSITION {paddle.paddle_pos.x} ")
-            print(f"BALL X POSITION = {self.ball_pos.x}")
-            
+
+
     def check_if_ball_hit_paddle(self, paddle: PaddleRect):
         self.ball_hit_left_side_paddle(paddle)
         self.ball_hit_right_side_paddle(paddle)
             
     
     def check_if_ball_hit_top_bottom_edges(self):
-        if self.ball_pos.y < 0:
+        if self.ball_pos.y <= 0:
             self.ball_speed[1] *= -1
             self.ball_speed[0] *= 1
         elif self.ball_pos.y > 720:
@@ -51,14 +55,12 @@ class GameBall:
         pass
 
     def move_ball_diagonal_left_up(self):
-        print("HIT LEFT PADDLE")
         self.ball_speed[1] *= -1
         self.ball_speed[0] *= -1
 
     def move_ball_diagonal_right_up(self):
-        print("HIT RIGHT PADDLE")
         self.ball_speed[1] *= -1
-        self.ball_speed[0] *= -1
+        self.ball_speed[0] *= 1
 
 
   
