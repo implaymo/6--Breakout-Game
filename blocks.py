daca786215d2072a9ad2cb1f3370954765c3143a
implaymo <136201112+implaymo@users.noreply.github.com>
2024-03-15 14:29:24 +0000

    Add self.all_blocks list to store all the blocks created in order to use it on my delete_block method to delete the blocks.

--- blocks.py
+++ blocks.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+from ball import GameBall
 
 class Block():
     def __init__(self) -> None:
@@ -8,14 +9,18 @@ class Block():
         self.rows = 4
         self.columns = 15
         self.get_random_color()
+
         
     def draw_multiple_blocks(self,screen):
+        self.all_blocks = []
         for row in range(self.rows):
             for column in range(self.columns):
                 player_pos_x = 0 + column * (self.block_width + 5)
                 player_pos_y = 100 + row * (self.block_height + 5)
-                pygame.draw.rect(screen, self.color, pygame.Rect(player_pos_x, player_pos_y, self.block_width, self.block_height))
-
+                new_block = pygame.draw.rect(screen, self.color, pygame.Rect(player_pos_x, player_pos_y, self.block_width, self.block_height))
+                self.all_blocks.append(new_block) 
+        return self.all_blocks
+    
     def get_random_color(self):
         red = random.randint(0, 255)
         green = random.randint(0, 255)
@@ -27,3 +32,10 @@ class Block():
             self.color = (red, green, blue)
         else:
             self.color = (red, green, blue)
+            
+    def delete_block(self, ball: GameBall):
+        for i in self.all_blocks:
+            block = self.all_blocks[i]
+            if block == ball.ball_pos:
+                self.all_blocks.pop(block)
+        
