7f6fda581261c630e874369c13861a4370441029
implaymo <goncalomorerasilva@gmail.com>
2024-03-23 19:32:30 +0000

    level.py needs fixing. Had to clear imported modules to make game work again.

--- level.py
+++ level.py
@@ -1,12 +1,9 @@
 import pygame
-from paddle import PaddleRect
-from blocks import Block
 
 class NewLevel:
     def __init__(self) -> None:
         self.level = 1
 
-    def change_level(self, paddle: PaddleRect, block: Block):
+    def change_level(self):
         self.level += 1
-        paddle.change_size_of_paddle(block)
         
